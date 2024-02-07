f82a06dc57796b24baeb6043e5152f6710ff5ec7
asura7969 <1402357969@qq.com>
2024-02-07 22:42:48 +0800

    header style

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,38 @@
     <n-space vertical size="large" style="width: 100%; height: 100%" item-style="width: 100%; height: 100%">
       <n-layout position="absolute" style="width: 100%; height: 100%">
         <n-layout-header 
-            position="static"
             bordered
             style="width: 100%; height: 65px"
           >
-            <n-grid x-gap="12" :cols="6">
-              <n-grid-item :offset="0" style="align-items: center; display: flex; height: 65px">
-                <n-flex>
-                  <n-image
-                    height="60"
-                    width="60"
-                      src="	https://seatunnel.apache.org/image/logo.png"
-                    />
-                  <n-gradient-text
-                    gradient="linear-gradient(130deg,#24c6dc,#5433ff 41.07%,#f09 76.05%)"
-                  >
-                    <h3>Seathnnel</h3>
-                  </n-gradient-text>
-                </n-flex>
-              </n-grid-item>
-              <n-grid-item :offset="4" style="align-items: center; display: flex;">
-                <n-flex justify="end">
-                  <n-button :bordered="false" :render-icon="themeIcon" @click="clickTheme" style="margin-left: 100px;">
-                    主题
+          <n-space :size="20" align="center" style="line-height: 1">
+            <n-image
+              height="60"
+              width="60"
+                src="	https://seatunnel.apache.org/image/logo.png"
+              />
+            <n-gradient-text
+              gradient="linear-gradient(130deg,#24c6dc,#5433ff 41.07%,#f09 76.05%)"
+            >
+              <h3>Seathnnel</h3>
+            </n-gradient-text>
+
+            <n-popover trigger="hover">
+              <template #trigger>
+                <n-button :bordered="false" :render-icon="themeIcon" @click="clickTheme">
                   </n-button>
-                </n-flex>
-              </n-grid-item>
-            </n-grid>
+              </template>
+              <span>主题</span>
+            </n-popover>
+            <n-popover trigger="hover">
+              <template #trigger>
+                <n-icon size="20" @click="screenFull">
+                    <FullscreenOutlined />
+                  </n-icon>
+              </template>
+              <span>全屏</span>
+            </n-popover>
+                  
+          </n-space>
           </n-layout-header>
         
         <n-layout has-sider style="width: 100%; height: calc(100vh - 65px)">
@@ -55,9 +60,6 @@
               label-field="label"
               children-field="children"
             />
-            <!-- key-field="key"
-              label-field="label"
-              children-field="children" -->
           </n-layout-sider>
           <n-layout-content content-style="padding: 24px;" :native-scrollbar="false">
             <n-message-provider>
@@ -72,13 +74,14 @@
 
 
 <script>
-// import Layout from './components/Layout.vue'
 import { defineComponent, h, ref, toRef } from "vue";
 import { darkTheme } from 'naive-ui'
 import { RouterLink, useRoute } from 'vue-router'
 import { routes } from './router'
 import { Sun } from '@vicons/tabler'
 import { WeatherMoon20Filled } from '@vicons/fluent'
+import { FullscreenOutlined } from '@vicons/antd'
+import screenfull from 'screenfull'
 
 function renderIcon(icon) {
   return () => h(NIcon, null, { default: () => h(icon) });
@@ -106,6 +109,7 @@ export default defineComponent({
   components: {
     Sun,
     WeatherMoon20Filled,
+    FullscreenOutlined,
   },
   setup() {
     const containerRef = ref(void 0);
@@ -131,6 +135,12 @@ export default defineComponent({
       }
     };
 
+    const screenFull = () => {
+      if (screenfull.isEnabled) {
+        screenfull.toggle()
+      }
+    };
+
     return {
       collapsed: ref(false),
       route: useRoute(),
@@ -138,7 +148,8 @@ export default defineComponent({
       containerRef,
       theme,
       themeIcon,
-      clickTheme
+      clickTheme,
+      screenFull,
     };
   },
   methods: {
@@ -152,6 +163,19 @@ export default defineComponent({
 
 
 <style scoped lang="scss">
+.n-layout-header {
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  padding: 9px 18px;
+}
+
+.n-space {
+  margin-left: auto;
+}
+
 .n-image {
   padding-left: 20px;
 }
@@ -160,5 +184,11 @@ export default defineComponent({
   ::v-deep(.n-layout-toggle-button){
     top: 10%
   }
+}
+
+.n-icon {
+  top: 50%;
+  align-items: center;
+  display: flex;
 }
 </style>
