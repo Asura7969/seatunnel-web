d5e5e621bd8180f8989757697e7fc80727c67e17
asura7969 <1402357969@qq.com>
2024-02-17 13:21:44 +0800

    dashboard card

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,23 +1,28 @@
 <template>
-  <n-row>
-    <n-col :span="12">
-      <n-statistic label="集群节点" :value="3">
-        <template #prefix>
-          <n-icon>
-            <md-save />
-          </n-icon>
-        </template>
-        <template #suffix>
-          / 3
-        </template>
-      </n-statistic>
-    </n-col>
-    <n-col :span="12">
-      <n-statistic label="任务数">
-        1,234,123
-      </n-statistic>
-    </n-col>
-  </n-row>
+  <n-space vertical :size="16">
+    <n-card :bordered="true" :hoverable="true" :embedded="true" class="card-wrapper">
+      <n-row>
+        <n-col :span="12">
+          <n-statistic label="集群节点" :value="3">
+            <template #prefix>
+              <n-icon>
+                <md-save />
+              </n-icon>
+            </template>
+            <template #suffix>
+              / 3
+            </template>
+          </n-statistic>
+        </n-col>
+        <n-col :span="12">
+          <n-statistic label="任务数">
+            1,234,123
+          </n-statistic>
+        </n-col>
+      </n-row>
+    </n-card>
+  </n-space>
+  
 </template>
 
 <script>
